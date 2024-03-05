353c414023ccf54f2b808c5d26e4b4870b4982dd
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2024-03-06 00:51:23 +0530

    api key added

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,26 @@
-import React from "react";
+import { useEffect } from "react";
 import Header from "./Header";
+import { API_OPTIONS } from "../utils/constants";
 
 const Browse = () => {
+  const getNowPlayingMovies = async () => {
+    try {
+      const data = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+        API_OPTIONS
+      );
+
+      const json = await data.json();
+      console.log(json);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+
   console.log("--broewsee");
   return (
     <div>
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNzk1Y2U3ZmYwYTE2YTcxMDAzZTU4Y2U2ZjYzMzMyMCIsInN1YiI6IjY1ZTc0MmVlMDlkZGE0MDE3Y2JlY2MxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pf-EEAADSv33UYlfraauw5yv0cc4NuuifagkNk_SRuo",
+  },
+};
