8bbf87079422426c1fc6bfcf055bcff855e0c9e8
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2024-03-13 00:41:02 +0530

    style: x-scrollbar hidden

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,10 +1,11 @@
 import MovieCard from "./MovieCard";
+import "./MovieList.css";
 
 const MovieList = ({ title, movies }) => {
   return (
     <div className="px-6 ">
       <h1 className="text-lg md:text-3xl py-4 text-white">{title}</h1>
-      <div className="flex overflow-x-scroll">
+      <div className="horizontallist no-scrollbar">
         <div className="flex">
           {movies?.map((movie) => (
             <MovieCard key={movie.id} posterPath={movie.poster_path} />
